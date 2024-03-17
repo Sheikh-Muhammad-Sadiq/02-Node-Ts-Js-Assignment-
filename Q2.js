01ca876e2674812fb45e0948742f6be99c464224
Sheikh-Muhammad-Sadiq <162907621+Sheikh-Muhammad-Sadiq@users.noreply.github.com>
2024-03-17 02:57:19 -0700

    Add files via upload
    Q 02 Node Ts-Js of 45 Assignments

---
+++ Q2.js
@@ -0,0 +1,5 @@
+"use strict";
+// 2. Personal Message: Strore a person's name in a variable, and print a message to that person. Your message
+//   to should be sample, such as, "Hello Eric, whould you like to learn some Python today?"
+let firstName = "Eric";
+console.log(`Hello ${firstName}, "whould you like to learn some Python today?"`);
---
+++ Q2.ts
@@ -0,0 +1,6 @@
+// 2. Personal Message: Strore a person's name in a variable, and print a message to that person. Your message
+//   to should be sample, such as, "Hello Eric, whould you like to learn some Python today?"
+
+let firstName: string = "Eric";
+
+console.log(`Hello ${firstName}, "whould you like to learn some Python today?"`)
